46b7d583e5fa642140de59203d3ccc4e7f535028
Javier Sánchez Rois <jsanchez@gradiant.org>
2017-04-03 16:30:55 +0200

    Failing test case for Splitter

--- test/splitter.test.cpp
+++ test/splitter.test.cpp
@@ -3,3 +3,16 @@
 //
 
 #include <catch/catch.hpp>
+#include <InputSplitter.h>
+using namespace std::string_literals;
+using namespace Catch::Matchers;
+
+TEST_CASE("Splitter","[unit]") {
+  InputSplitter splitter;
+
+  SECTION("separates flags and parameters from input text") {
+    auto segments = splitter.split({"./myApp", "-v", "--input-file=input.pdf"});
+    CHECK_THAT(segments, VectorContains(std::vector<std::string>{"v"}));
+    CHECK_THAT(segments, VectorContains(std::vector<std::string>{"input-file","input.pdf"}));
+  }
+}
