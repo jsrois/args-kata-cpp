36435e5603d6dd089a2123391a529d3a85da05fb
Javier Sánchez Rois <jsanchez@gradiant.org>
2017-04-03 15:56:23 +0200

    Kind of refactoring for CommandLineParser. (not really). Don't like the OptionSet::setFlag and ::setParameter routine Also, separated the test in sections

--- test/cli.test.cpp
+++ test/cli.test.cpp
@@ -11,9 +11,9 @@ using namespace std::string_literals;
 TEST_CASE("CommandLineParser","[unit]") {
   Mock<InputSplitter> splitterMock;
   When(Method(splitterMock,split)).AlwaysReturn({
-    {"v"},
-    {"input-file","input.pdf"}
-  });
+                                                    {"v"},
+                                                    {"input-file","input.pdf"}
+                                                });
   Mock<OptionSet> fakeOptionSet;
   Fake(Method(fakeOptionSet,add));
   Fake(Method(fakeOptionSet,setFlag));Fake(Method(fakeOptionSet,setParameter));
@@ -27,24 +27,29 @@ TEST_CASE("CommandLineParser","[unit]") {
 
   Schema schema = {verboseRule,inputRule};
 
-  const char * m_argv[] = {"./myApp","-v","--input-file=input.pdf"};
-  const char ** argv = m_argv; int argc = 3; // C sucks, be careful when changing this number
-
   parser.addSchema(schema);
 
-  Verify(Method(fakeOptionSet,add).Using(verboseRule)).Once();
-  Verify(Method(fakeOptionSet,add).Using(inputRule)).Once();
+  Verify(Method(fakeOptionSet, add).Using(verboseRule)).Once();
+  Verify(Method(fakeOptionSet, add).Using(inputRule)).Once();
 
-  parser.parse(argc,argv);
+  SECTION("parses input parameters") {
+    struct {
+      int argc = 3; // C sucks, be careful when changing this number
+      const char *m_argv[3] = {"./myApp", "-v", "--input-file=input.pdf"};
+      const char **argv = m_argv;
+    } goodInput;
 
-  Verify(Method(splitterMock,split)
-             .Using(/*std::vector*/{"./myApp","-v","--input-file=input.pdf"}))
-      .Once();
+    parser.parse(goodInput.argc, goodInput.argv);
 
-  Verify(Method(fakeOptionSet,setFlag).Using("v"));
-  Verify(Method(fakeOptionSet,setParameter).Using("input-file","input.pdf"));
+    Verify(Method(splitterMock, split)
+               .Using(/*std::vector*/{"./myApp", "-v", "--input-file=input.pdf"}))
+        .Once();
 
-  CHECK(parser.getFlag("verbose"));
-  CHECK(parser.getParameter("input-file") == "input.pdf"s);
+    Verify(Method(fakeOptionSet, setFlag).Using("v"));
+    Verify(Method(fakeOptionSet, setParameter).Using("input-file", "input.pdf"));
+
+    CHECK(parser.getFlag("verbose"));
+    CHECK(parser.getParameter("input-file") == "input.pdf"s);
+  }
 }
 
