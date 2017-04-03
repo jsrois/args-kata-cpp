5b669f0c44b472c8e4aed31b9c1361cf1cf7fe2a
Javier Sánchez Rois <jsanchez@gradiant.org>
2017-04-03 17:23:40 +0200

    Test passing. Now, refactor and then add more test data to triangulate.

--- src/InputSplitter.cpp
+++ src/InputSplitter.cpp
@@ -3,6 +3,25 @@
 //
 
 #include "InputSplitter.h"
-std::vector<std::vector<std::string>> InputSplitter::split(std::vector<std::string> input) {
-  return std::vector<std::vector<std::string>>();
+#include <algorithm>
+using namespace std;
+
+vector<vector<string>> InputSplitter::split(vector<string> tokens) {
+  vector<vector<string>> segments;
+  for (const string token : tokens) {
+    string dashes = "--"s;
+    if (equal(dashes.begin(),dashes.end(),token.begin())) {
+      // remove prefix from string c++
+      string name, value;
+      name = token.substr(2);
+      auto equalsPos = name.find_first_of("=");
+      value = name.substr(equalsPos+1);
+      name = name.substr(0,equalsPos);
+      segments.push_back({name,value});
+    } else if (equal(dashes.begin(),dashes.begin(),token.begin())) {
+      auto name = token.substr(1); // remove leading dash
+      segments.push_back({name});
+    }
+  }
+  return segments;
 }
